1f44ac88d6ef272bb9d9f23047808183690ef41c
sunhmk <yijiantongming@163.com>
2017-07-26 15:21:53 +0800

    add project base-angularjs

---
+++ base-shiro/src/main/java/org/base/shiro/springfromdb/LoginController.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package org.base.shiro.springfromdb;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.web.util.WebUtils;
+import org.base.shiro.ini.QuickStart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.common.collect.Maps;
+
+/**
+ * 登录Controller
+ * @author ThinkGem
+ * @version 2013-5-31
+ */
+@Controller
+public class LoginController{
+    private static final transient Logger logger = LoggerFactory.getLogger(LoginController.class);
+	/**
+	 * 管理登录
+	 */
+	@RequestMapping(value = "${adminPath}/login", method = RequestMethod.GET)
+	public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
+	
+//		String view;
+//		view = "/WEB-INF/views/modules/sys/sysLogin.jsp";
+//		view = "classpath:";
+//		view += "jar:file:/D:/GitHub/jeesite/src/main/webapp/WEB-INF/lib/jeesite.jar!";
+//		view += "/"+getClass().getName().replaceAll("\\.", "/").replace(getClass().getSimpleName(), "")+"view/sysLogin";
+//		view += ".jsp";
+		return "modules/sys/sysLogin";
+	}
+
+	/**
+	 * 登录失败，真正登录的POST请求由Filter完成
+	 */
+	@RequestMapping(value = "${adminPath}/login", method = RequestMethod.POST)
+	public String loginFail(HttpServletRequest request, HttpServletResponse response, Model model) {
+		
+		
+		return "modules/sys/sysLogin";
+	}
+
+	/**
+	 * 登录成功，进入管理首页
+	 */
+	@RequiresPermissions("user")
+	@RequestMapping(value = "${adminPath}")
+	public String index(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+//		// 登录成功后，获取上次登录的当前站点ID
+//		UserUtils.putCache("siteId", StringUtils.toLong(CookieUtils.getCookie(request, "siteId")));
+
+//		System.out.println("==========================a");
+//		try {
+//			byte[] bytes = com.thinkgem.jeesite.common.utils.FileUtils.readFileToByteArray(
+//					com.thinkgem.jeesite.common.utils.FileUtils.getFile("c:\\sxt.dmp"));
+//			UserUtils.getSession().setAttribute("kkk", bytes);
+//			UserUtils.getSession().setAttribute("kkk2", bytes);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+////		for (int i=0; i<1000000; i++){
+////			//UserUtils.getSession().setAttribute("a", "a");
+////			request.getSession().setAttribute("aaa", "aa");
+////		}
+//		System.out.println("==========================b");
+		return "modules/sys/sysIndex";
+	}
+}
